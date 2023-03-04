6d647de4d496e9a62abf7cdba3772df4f6b2e7f2
DefiCody <ambergridpro@gmail.com>
2023-03-04 19:10:12 +0530

    test

--- src/pages/partnerships/index.js
+++ src/pages/partnerships/index.js
@@ -32,7 +32,7 @@ const WhitepaperPdf = () => {
             <Grid container spacing={2} className='match-height' sx={{ m: 5 }}> 
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
 
-                        <Viewer fileUrl="/Pathernships_2.pdf"
+                        <Viewer fileUrl="/Pathernships_.pdf"
                             renderPage={renderPage}
                             />
                     </Worker> 
